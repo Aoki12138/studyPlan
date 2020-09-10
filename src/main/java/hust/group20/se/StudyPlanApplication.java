ec30fe13f97ceab1a019393f318358052bfdc3ca
aoki <932877796@qq.com>
2020-09-10 11:03:41 +0800

    INIT DONE

---
+++ src/main/java/hust/group20/se/StudyPlanApplication.java
@@ -0,0 +1,13 @@
+package hust.group20.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hust/group20/se/StudyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package hust.group20.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
