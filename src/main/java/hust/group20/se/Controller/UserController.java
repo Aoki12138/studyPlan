905b863611f5a702a93de4a174ff938ff5796bb7
aoki <932877796@qq.com>
2020-09-14 15:40:12 +0800

    TEST RETURN INDEX

--- src/main/java/hust/group20/se/Controller/UserController.java
+++ src/main/java/hust/group20/se/Controller/UserController.java
@@ -1,7 +1,24 @@
 package hust.group20.se.Controller;
 
+import hust.group20.se.Service.TaskService;
+import hust.group20.se.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TaskService taskService;
+
+    //登录之后才能返回用户主页面！
+    @RequestMapping("/index")
+    public String showMainPage(){
+        return "index";
+    }
+
+
 }
