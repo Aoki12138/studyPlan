5dfc8b473634d7239a9f5874ef525cf6753be4f6
WJ <1041213687@qq.com>
2020-09-14 10:31:31 +0800

    ADD RESOURCES

---
+++ src/main/resources/static/js/pages/charts/chartjs.js
@@ -0,0 +1,109 @@
+$(function () {
+    new Chart(document.getElementById("line_chart_chartjs").getContext("2d"), getChartJs('line'));
+    new Chart(document.getElementById("bar_chart_chartjs").getContext("2d"), getChartJs('bar'));
+    new Chart(document.getElementById("radar_chart_chartjs").getContext("2d"), getChartJs('radar'));
+    new Chart(document.getElementById("pie_chart_chartjs").getContext("2d"), getChartJs('pie'));
+});
+
+function getChartJs(type) {
+    var config = null;
+
+    if (type === 'line') {
+        config = {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    borderColor: 'rgba(0, 188, 212, 0.75)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    pointBorderWidth: 1
+                }, {
+                        label: "My Second dataset",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        borderColor: 'rgba(233, 30, 99, 0.75)',
+                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        pointBorderWidth: 1
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'bar') {
+        config = {
+            type: 'bar',
+            data: {
+                labels: ["Subject1", "Subject2", "Subject3", "Subject4", "Subject5", "Subject6", "Subject7"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    backgroundColor: 'rgba(0, 188, 212, 0.8)'
+                }, {
+                        label: "My Second dataset",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        backgroundColor: 'rgba(233, 30, 99, 0.8)'
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'radar') {
+        config = {
+            type: 'radar',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July","none"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 25, 90, 81, 56, 55, 40,50],
+                    borderColor: 'rgba(0, 188, 212, 0.8)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.5)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.8)',
+                    pointBorderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'pie') {
+        config = {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [225, 50, 100, 40],
+                    backgroundColor: [
+                        "rgb(233, 30, 99)",
+                        "rgb(255, 193, 7)",
+                        "rgb(0, 188, 212)",
+                        "rgb(139, 195, 74)"
+                    ],
+                }],
+                labels: [
+                    "Pink",
+                    "Amber",
+                    "Cyan",
+                    "Light Green"
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    return config;
+}
